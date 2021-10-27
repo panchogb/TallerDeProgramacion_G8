a830dfe66d8416212e07380d1783c65d2e741522
francisco <franciscogbmdp@gmail.com>
2021-10-27 19:36:16 -0300

    version con programa a testear

---
+++ TP - Caja negra/proyecto/src/Prueba/Prueba.java
@@ -0,0 +1,12 @@
+package Prueba;
+
+import lanzador.Lanzador;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lanzador.main(args);
+	}
+
+}
--- TP - Programación Defensiva - Proceso Disciplinado - Aserciones/Caso 1 - Una cerveceria/src/Prueba/Prueba.java
+++ TP - Programación Defensiva - Proceso Disciplinado - Aserciones/Caso 1 - Una cerveceria/src/Prueba/Prueba.java
@@ -6,6 +6,12 @@ import Vista.Ventana;
 public class Prueba {
 	
 	public static void main(String[] args) {		
+
+		// TODO Auto-generated method stub
+		String h = "Hola";
+		String ho = "hola";
+		
+		System.out.println(h.equals(ho));
 		Negocio ventana = new Negocio(new Ventana()); 
 	}
 
